6f6edb6a979a5288202a639e22b59810b0f4db3e
MoemenMekadmini <86653152+MoemenMekadmini@users.noreply.github.com>
2021-07-19 10:16:40 +0100

    Add files via upload

---
+++ checkpoint.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def binarySearch(alist, item):
+    first = 0
+    last = len(alist)-1
+    found = False
+
+    while first<=last and not found:
+        midpoint = (first + last)//2
+        if alist[midpoint] == item:
+            found = True
+        else:
+            if item < alist[midpoint]:
+                last = midpoint-1
+            else:
+                first = midpoint+1
+    return found
+binary_search=[1,2,3,5,8]
+print(binarySearch(binary_search, 6))
+
+
+# In[4]:
+
+
+def power(a,b):
+    P=a**b
+    return P
+power(3,4)
+
+
+# In[5]:
+
+
+def bubbleSort(alist):
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+Sample_Data=[29,13,22,37,52,49,46,71,56]
+bubbleSort(Sample_Data)
+print(Sample_Data)
+
+
+# In[7]:
+
+
+def mergeSort(alist):
+    if len(alist)>1:
+        mid = len(alist)//2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i=0
+        j=0
+        k=0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] <= righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+Sample_Data=[29,13,22,37,52,49,46,71,56]
+mergeSort(Sample_Data)
+print(Sample_Data)
+
+
+# In[8]:
+
+
+def quickSort(alist):
+    quickSortHelper(alist,0,len(alist)-1)
+    
+def quickSortHelper(alist,first,last):
+    if first<last:
+
+        splitpoint = partition(alist,first,last)
+
+        quickSortHelper(alist,first,splitpoint-1)
+        quickSortHelper(alist,splitpoint+1,last)
+
+
+def partition(alist,first,last):
+    pivotvalue = alist[first]
+
+    leftmark = first+1
+    rightmark = last
+
+    done = False
+    while not done:
+
+        while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+            leftmark = leftmark + 1
+
+        while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+            rightmark = rightmark -1
+
+        if rightmark < leftmark:
+            done = True
+        else:
+            temp = alist[leftmark]
+            alist[leftmark] = alist[rightmark]
+            alist[rightmark] = temp
+ 
+    temp = alist[first]
+    alist[first] = alist[rightmark]
+    alist[rightmark] = temp
+
+
+    return rightmark
+Sample_Data=[29,13,22,37,52,49,46,71,56]
+quickSort(Sample_Data)
+print(Sample_Data)
+
+
+# In[ ]:
+
+
+
+
